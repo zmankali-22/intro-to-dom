4387330f636c428b6df5439bd28bab7315ce7cd9
acharya <sav.acharya22@gmail.com>
2024-04-16 19:30:37 +1000

    animal list

---
+++ favouriteAnimal.js
@@ -0,0 +1,17 @@
+let animals = [
+  "cat",
+  "koala",
+  "turtle",
+  "drop bear",
+  "tiger",
+  "rat",
+  "lion",
+  "tiger",
+  "fox",
+];
+
+function createAnimalList() {
+    animals.forEach((animal) => {
+        console.log("animal", animal)
+    })
+}
